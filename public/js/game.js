88e593e9c50e264846cd85323afd923ed350a493
lelex22 <alpedros@ucm.es>
2020-02-26 18:59:13 +0100

    Introducido mapa (nada mas). Se ve grande

--- public/js/game.js
+++ public/js/game.js
@@ -1,14 +1,13 @@
-var config = {
-  width: 800,
-  height: 600, 
-  scene: [scene1,scene2],
-  pixelArt: true,
-  physics: {
-    default: 'arcade',
-    arcade: {
-        gravity: { y: 200 }
+export default class Game extends Phaser.Scene {
+    constructor() {
+      super({ key: 'main' });
     }
-  },
-}
-
-var game = new Phaser.Game(config);
+    preload() {  
+    }
+  
+    create() {
+    }
+  
+    update(time, delta) {    
+    }
+  }

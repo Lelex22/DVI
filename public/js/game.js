8da25dba689c561d1f14beab9d027b93f9ab5d74
lelex22 <alpedros@ucm.es>
2020-02-28 20:19:11 +0100

    Mazmorra extraña

--- public/js/game.js
+++ public/js/game.js
@@ -1,13 +1,22 @@
-export default class Game extends Phaser.Scene {
-    constructor() {
-      super({ key: 'main' });
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  backgroundColor: '#2a2a55',
+  parent: 'phaser-example',
+  pixelArt: true,
+  roundPixels: false,
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 500 }
     }
-    preload() {  
-    }
-  
-    create() {
-    }
-  
-    update(time, delta) {    
-    }
+  },
+  scene: {
+      preload: preload,
+      create: create,
+      update: update
   }
+};
+
+const game = new Phaser.Game(config);

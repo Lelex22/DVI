73f4c9c7fac3e5485b494f174b7f0ec611934a41
lelex22 <alpedros@ucm.es>
2020-03-25 12:54:40 +0100

    Modificado docs

---
+++ docs/js/dungeon-scene.js
@@ -0,0 +1,222 @@
+
+//import Dungeon from "@mikewesthad/dungeon";
+
+// Codigo utilizado desde https://github.com/mikewesthad/phaser-3-tilemap-blog-posts/blob/master/posts/post-3/demo/js/tile-mapping.js
+import Player from "./player.js";
+import TILES from "./tile-mapping.js";
+import TilemapVisibility from "./tilemap-visibility.js";
+
+/**
+ * Scene that generates a new dungeon
+ */
+export default class DungeonScene extends Phaser.Scene {
+  constructor() {
+    super('DungeonScene');
+    this.level = 0;
+    
+  }
+  init(data){
+    if(data !== null){
+      console.log("Done");
+      this.lifesPlayer = data.vidas;
+      this.coinsPlayer = data.monedas;
+      this.buffsPlayer = data.buffs;
+    }
+    //else this.player = new Player(this, 0, 0);
+  }
+  
+
+  preload() {
+    this.load.image("tiles", "../public/assets/tilesets/prueba2.png");
+    this.load.image("heart", "../public/img/heart.png");
+    this.load.spritesheet(
+      "characters",
+      "../public/assets/spritesheets/luigi-sprites.png",
+      {
+        frameWidth: 28,
+        frameHeight: 28
+      }
+    );
+  }
+
+  create() {
+    
+    this.level++;
+    this.hasPlayerReachedShop = false;
+
+    // Generate a random world with a few extra options:
+    //  - Rooms should only have odd number dimensions so that they have a center tile.
+    //  - Doors should be at least 2 tiles away from corners, so that we can place a corner tile on
+    //    either side of the door location
+    this.dungeon = new Dungeon({
+      width: 50,
+      height: 50,
+      doorPadding: 2,
+      rooms: {
+        width: { min: 7, max: 15, onlyOdd: true },
+        height: { min: 7, max: 15, onlyOdd: true }
+      }
+    });
+
+    //this.dungeon.drawToConsole();
+
+    // Creating a blank tilemap with dimensions matching the dungeon
+    const map = this.make.tilemap({
+      tileWidth: 32,
+      tileHeight: 32,
+      width: this.dungeon.width,
+      height: this.dungeon.height
+    });
+    const tileset = map.addTilesetImage("tiles", null, 32, 32, 0, 0); // 1px margin, 2px spacing
+    this.groundLayer = map.createBlankDynamicLayer("Ground", tileset).fill(TILES.BLANK);
+    this.stuffLayer = map.createBlankDynamicLayer("Stuff", tileset);
+    const shadowLayer = map.createBlankDynamicLayer("Shadow", tileset).fill(TILES.BLANK);
+
+    this.tilemapVisibility = new TilemapVisibility(shadowLayer);
+
+    // Use the array of rooms generated to place tiles in the map
+    // Note: using an arrow function here so that "this" still refers to our scene
+    this.dungeon.rooms.forEach(room => {
+      const { x, y, width, height, left, right, top, bottom } = room;
+
+      // Fill the floor with mostly clean tiles, but occasionally place a dirty tile
+      // See "Weighted Randomize" example for more information on how to use weightedRandomize.
+      this.groundLayer.weightedRandomize(x + 1, y + 1, width - 2, height - 2, TILES.FLOOR);
+
+      // Place the room corners tiles
+      this.groundLayer.putTileAt(TILES.WALL.TOP_LEFT, left, top);
+      this.groundLayer.putTileAt(TILES.WALL.TOP_RIGHT, right, top);
+      this.groundLayer.putTileAt(TILES.WALL.BOTTOM_RIGHT, right, bottom);
+      this.groundLayer.putTileAt(TILES.WALL.BOTTOM_LEFT, left, bottom);
+
+      // Fill the walls with mostly clean tiles, but occasionally place a dirty tile
+      this.groundLayer.weightedRandomize(left + 1, top, width - 2, 1, TILES.WALL.TOP);
+      this.groundLayer.weightedRandomize(left + 1, bottom, width - 2, 1, TILES.WALL.BOTTOM);
+      this.groundLayer.weightedRandomize(left, top + 1, 1, height - 2, TILES.WALL.LEFT);
+      this.groundLayer.weightedRandomize(right, top + 1, 1, height - 2, TILES.WALL.RIGHT);
+
+      // Dungeons have rooms that are connected with doors. Each door has an x & y relative to the
+      // room's location
+      var doors = room.getDoorLocations();
+      for (var i = 0; i < doors.length; i++) {
+        if (doors[i].y === 0) {
+          this.groundLayer.putTilesAt(TILES.DOOR.TOP, x + doors[i].x - 1, y + doors[i].y);
+        } else if (doors[i].y === room.height - 1) {
+          this.groundLayer.putTilesAt(TILES.DOOR.BOTTOM, x + doors[i].x - 1, y + doors[i].y);
+        } else if (doors[i].x === 0) {
+          this.groundLayer.putTilesAt(TILES.DOOR.LEFT, x + doors[i].x, y + doors[i].y - 1);
+        } else if (doors[i].x === room.width - 1) {
+          this.groundLayer.putTilesAt(TILES.DOOR.RIGHT, x + doors[i].x, y + doors[i].y - 1);
+        }
+      }
+    });
+
+    // Separate out the rooms into:
+    //  - The starting room (index = 0)
+    //  - A random room to be designated as the end room (with stairs and nothing else)
+    //  - An array of 90% of the remaining rooms, for placing random stuff (leaving 10% empty)
+    const rooms = this.dungeon.rooms.slice();
+    const startRoom = rooms.shift();
+    const endRoom = Phaser.Utils.Array.RemoveRandomElement(rooms);
+    const otherRooms = Phaser.Utils.Array.Shuffle(rooms).slice(0, rooms.length * 0.9);
+
+    // Place the shop
+    this.stuffLayer.putTileAt(TILES.SHOP, endRoom.centerX, endRoom.centerY);
+
+    // Place stuff in the 90% "otherRooms"
+    otherRooms.forEach(room => {
+      var rand = Math.random();
+      if (rand <= 0.25) {
+        // 25% chance of chest
+        this.stuffLayer.putTileAt(TILES.CHEST, room.centerX, room.centerY);
+      } else if (rand <= 0.5) {
+        // 50% chance of a pot anywhere in the room... except don't block a door!
+        const x = Phaser.Math.Between(room.left + 2, room.right - 2);
+        const y = Phaser.Math.Between(room.top + 2, room.bottom - 2);
+        this.stuffLayer.weightedRandomize(x, y, 1, 1, TILES.POT);
+      } else {
+        // 25% of either 2 or 4 towers, depending on the room size
+        if (room.height >= 9) {
+          this.stuffLayer.putTilesAt(TILES.TOWER, room.centerX - 1, room.centerY + 1);
+          this.stuffLayer.putTilesAt(TILES.TOWER, room.centerX + 1, room.centerY + 1);
+          this.stuffLayer.putTilesAt(TILES.TOWER, room.centerX - 1, room.centerY - 2);
+          this.stuffLayer.putTilesAt(TILES.TOWER, room.centerX + 1, room.centerY - 2);
+        } else {
+          this.stuffLayer.putTilesAt(TILES.TOWER, room.centerX - 1, room.centerY - 1);
+          this.stuffLayer.putTilesAt(TILES.TOWER, room.centerX + 1, room.centerY - 1);
+        }
+      }
+    });
+
+    // Not exactly correct for the tileset since there are more possible floor tiles, but this will
+    // do for the example.
+    this.groundLayer.setCollisionByExclusion([-1, 4, 12]);
+    this.stuffLayer.setCollisionByExclusion([-1, 4, 12]);
+
+    this.stuffLayer.setTileIndexCallback(TILES.SHOP, () => {
+      this.stuffLayer.setTileIndexCallback(TILES.SHOP, null);
+      this.hasPlayerReachedShop = true;
+      this.player.freeze();
+      const cam = this.cameras.main;
+      cam.fade(250, 0, 0, 0);
+      cam.once("camerafadeoutcomplete", () => {
+        this.player.destroy();
+        this.scene.start("ShopScene", {vidas: this.player.life, monedas: this.player.coins, buffs: this.player.buffs});
+      });
+    });
+
+    // Place the player in the first room
+    const playerRoom = startRoom;
+    const x = map.tileToWorldX(playerRoom.centerX);
+    const y = map.tileToWorldY(playerRoom.centerY);
+    this.player = new Player(this, x, y);
+    if(this.lifesPlayer && (this.coinsPlayer || this.coinsPlayer === 0) && this.buffsPlayer){
+      this.buffsPlayer.forEach(function (elem, i){
+        if(elem.value) 
+          this.player.buffs[i].value = elem.value;
+      }, this);
+      this.player.buffs = this.buffsPlayer;
+      this.player.coins = this.coinsPlayer;
+      this.player.life = this.lifesPlayer;
+    }
+    // Watch the player and tilemap layers for collisions, for the duration of the scene:
+    this.physics.add.collider(this.player.sprite, this.groundLayer);
+    this.physics.add.collider(this.player.sprite, this.stuffLayer);
+
+    // Phaser supports multiple cameras, but you can access the default camera like this:
+    const camera = this.cameras.main;
+
+    // Constrain the camera so that it isn't allowed to move outside the width/height of tilemap
+    camera.setBounds(0, 0, map.widthInPixels, map.heightInPixels);
+    camera.startFollow(this.player.sprite);
+    /* Vidas: Se supone que ese this.life = 5 no va a ser necesario, sino que vamos
+    a obtener las vidas restantes */
+    
+    for(var i = 0; i < this.player.life; i++)
+      this.add.image(32 * i + 16, 20, 'heart').setScrollFactor(0);
+    
+    // Help text that has a "fixed" position on the screen
+    this.add.text(16, 460, `Find the shop or levels. \nBuff espada: ${this.player.buffs[1].value}. \nLife: ${this.player.life}`, {
+        font: "18px monospace",
+        fill: "#000000",
+        padding: { x: 20, y: 10 },
+        backgroundColor: "#ffffff"
+      })
+      .setScrollFactor(0);
+;
+  }
+
+  update(time, delta) {
+    if (this.hasPlayerReachedShop) return;
+
+    this.player.update();
+
+    // Find the player's room using another helper method from the dungeon that converts from
+    // dungeon XY (in grid units) to the corresponding room object
+    const playerTileX = this.groundLayer.worldToTileX(this.player.sprite.x);
+    const playerTileY = this.groundLayer.worldToTileY(this.player.sprite.y);
+    const playerRoom = this.dungeon.getRoomAt(playerTileX, playerTileY);
+
+    this.tilemapVisibility.setActiveRoom(playerRoom);
+  }
+}

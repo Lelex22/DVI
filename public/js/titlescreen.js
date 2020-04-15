1d8f7182c1c67a8489a8125639c6eb945e05f12c
lelex22 <alpedros@ucm.es>
2020-04-15 13:06:32 +0200

    Pantalla inicio basica

---
+++ public/js/titlescreen.js
@@ -0,0 +1,35 @@
+export default class TitleScreenScene extends Phaser.Scene {
+
+    constructor ()
+    {
+        super('TitleScreenScene');
+    }
+    preload(){
+        this.load.bitmapFont('nokia', '../public/assets/nokia16.png', '../public/assets/nokia16.xml');
+        this.load.image("background", '../public/img/title3.png');
+        this.load.image("background2", '../public/img/title4.png');
+        this.load.image("luigi", '../public/img/title2.png');
+    }
+    create(){
+        this.anims.create({
+            key: "run",
+            frames: [
+                {key: 'background'}, 
+                {key: 'background2'}
+            ],
+            frameRate: 2,
+            repeat: 0
+        });       
+
+        this.key = this.input.keyboard.addKey('ENTER');
+        this.background = this.add.sprite(400, 300, 'background').play('run');
+        this.add.image(400, 300, 'luigi');
+        this.add.dynamicBitmapText(100, 100, 'nokia', 'Zelgui', 128).setScrollFactor(0);
+    }
+    update(){
+        this.background.anims.play("run", true);
+        if(this.key.isDown){
+            this.scene.start("DungeonScene");
+        }
+    }
+}

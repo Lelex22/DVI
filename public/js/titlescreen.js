60ddc91e59d964acb5b39bcab3a6a960d4f148d9
jagoba05 <jml_1993@hotmail.es>
2020-04-15 15:32:29 +0200

    Añadido texto al menu de inicio

--- public/js/titlescreen.js
+++ public/js/titlescreen.js
@@ -20,13 +20,15 @@ export default class TitleScreenScene extends Phaser.Scene {
             frameRate: 2,
             repeat: 0
         });       
-
         this.key = this.input.keyboard.addKey('ENTER');
         this.background = this.add.sprite(400, 300, 'background').play('run');
         this.add.image(400, 300, 'luigi');
         this.add.dynamicBitmapText(100, 100, 'nokia', 'Zelgui', 128).setScrollFactor(0);
+        this.add.dynamicBitmapText(30, 500, 'nokia', 'Pulsa ENTER para iniciar', 30).setScrollFactor(0);
+        
     }
     update(){
+        
         this.background.anims.play("run", true);
         if(this.key.isDown){
             this.scene.start("DungeonScene");

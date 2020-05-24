09cbe14c3d016f91d8a24bcb84a43fcca29ae2f0
jagoba05 <jml_1993@hotmail.es>
2020-05-24 21:17:35 +0200

    Añadida parte de nivel 2

---
+++ public/assets/spritesheets/imageedit_13_7724971945.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.2" name="imageedit_13_7724971945" tilewidth="80" tileheight="80" tilecount="48" columns="8">
+ <image source="imageedit_13_7724971945.png" trans="ff0000" width="680" height="520"/>
+</tileset>

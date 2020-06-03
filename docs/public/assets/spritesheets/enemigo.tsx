929a1116db0cfd45fa01f83b2f789074585249a2
lelex22 <alpedros@ucm.es>
2020-06-03 12:19:51 +0200

    No docs

--- docs/public/assets/spritesheets/enemigo.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="enemigo" tilewidth="32" tileheight="32" tilecount="100" columns="10">
- <image source="enemigo.png" width="320" height="320"/>
-</tileset>
--- docs/public/assets/tilesets/Spritesheet_tileset.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.2" tiledversion="1.3.2" name="Spritesheet_tileset" tilewidth="16" tileheight="16" tilecount="3072" columns="64">
- <image source="Spritesheet_tileset.png" trans="ff0000" width="1024" height="768"/>
-</tileset>
